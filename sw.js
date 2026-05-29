b784c713a869ee189ede12eb245e994651028d58
ofiazadhan-source <ofiazadhan@gmail.com>
2026-05-29 19:54:33 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,40 @@
+const CACHE_NAME = 'dhan-pos-v2'; // nagpalit ako ng v2 para mag-force update
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/settings.html', 
+  '/products.html',
+  '/expenses.html',
+  '/firebase-config.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('dhan-pos-v1').then((cache) => {
-      return cache.addAll(['index.html', 'settings.html', 'firebase-config.js']);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if(cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
